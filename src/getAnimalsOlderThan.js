73546daa53250c9a3a6badd626ef15182baa50f0
RamosFonseca <beidseitig@gmail.com>
2022-01-31 10:55:25 -0300

    Requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan (animal, age) {
   let animals = data.species.find(({name}) => name === animal);
   return animals.residents.every((e) => e.age >= age);
-};
+}
 
 console.log(getAnimalsOlderThan('otters', 7));
 
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employeeDetails = employees.find((theEmployee) =>
+  (theEmployee.firstName === employeeName || theEmployee.lastName === employeeName));
+
+  if (employeeDetails === undefined) {
+    return {};
+  }
+
+  return employeeDetails;
 }
+
+console.log(getEmployeeByName('Nelson'))
 
 module.exports = getEmployeeByName;
