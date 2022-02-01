c2049b92a87d878004dbc29b46ef7a13e4177543
RamosFonseca <beidseitig@gmail.com>
2022-02-01 05:36:14 -0300

    REquisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const { employees } = data;
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const isManager = (id) => employees.some((post) => post.managers.find((checkId) => checkId === id));
+
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+
+const getRelatedEmployees = (managerId) => {
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return employees.filter((subordinate) =>
+      subordinate.managers.includes(managerId)).map((subordinate) =>
+      `${subordinate.firstName} ${subordinate.lastName}`);
+  }
+};
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
