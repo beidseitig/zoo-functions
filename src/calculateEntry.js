4d37b309c6c379cc53003d44888aac2da3d905d2
RamosFonseca <beidseitig@gmail.com>
2022-02-01 06:30:26 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const visitors = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach(({ age }) => {
+    if (age >= 50) { visitors.senior += 1; return; }
+    if (age >= 18) { visitors.adult += 1; return; }
+    visitors.child += 1;
+  });
+  return visitors;
+};
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const visitors = countEntrants(entrants);
+  const keys = Object.keys(visitors);
+
+  return keys.reduce((acc, i) => acc + data.prices[i] * visitors[i], 0);
+};
 
 module.exports = { calculateEntry, countEntrants };
