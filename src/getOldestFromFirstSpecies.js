1ef2e8ba9fcd5cf5c19e34b003355b8fadd314a6
RamosFonseca <beidseitig@gmail.com>
2022-02-01 06:45:26 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const olderSpecie = employees.find((animal) => animal.id === id).responsibleFor[0];
+  const specie = species.find((animal) => animal.id === olderSpecie)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(specie);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
